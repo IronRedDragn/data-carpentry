45b29f64d3a8ddb4a13dfe5c404a585ad6a83a2a
Tony <67649729+IronRedDragn@users.noreply.github.com>
2021-03-04 14:40:45 -0500

    edits to scripts to include cmd line arguments for gridlines,cbar lines, units

--- data-carpentry/plot_precipitation_climatology.py
+++ data-carpentry/plot_precipitation_climatology.py
@@ -1,42 +1,57 @@
-import argparse
 import xarray as xr
-import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 import numpy as np
 import cmocean
+import argparse
 
 
-def convert_pr_units(darray):
-    """Convert kg m-2 s-1 to mm day-1.
-    
+def convert_pr_units(darray, units=1):
+    """Convert kg m-2 s-1 to mm day-1 by default.
+
     Args:
       darray (xarray.DataArray): Precipitation data
-    
+    Kwargs:
+       unit (int): Tells which to convert units to. 
+                   0 = kg m-2 s-1 (does not convert units) 
+                   1 = mm/day (converts units to mm/day )
     """
-    
-    darray.data = darray.data * 86400
-    darray.attrs['units'] = 'mm/day'
-    
+    if units == 1:
+        darray.data = darray.data * 86400
+        darray.attrs['units'] = 'mm/day'
+    elif units == 2:
+        darray.data = darray.data * 3600
+        darray.attrs['units'] = 'mm/hr'
     return darray
 
 
-def create_plot(clim, model, season, gridlines=False):
+def create_plot(clim, model, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
-    
+
     Args:
       clim (xarray.DataArray): Precipitation climatology data
       model (str): Name of the climate model
       season (str): Season
-      
+
     Kwargs:
-      gridlines (bool): Select whether to plot gridlines    
-    
+      gridlines (bool): Select whether to plot gridlines
+      levels (list): Tick marks on the colorbar    
+
     """
-        
-    fig = plt.figure(figsize=[12,5])
-    ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
+
+    if not levels:
+        if clim.units == 'mm/day':
+            levels = np.arange(0, 13.5, 1.5)
+        elif clim.units == 'mm/hr':
+            levels = np.arange(0, 0.6, 0.015)
+        else:
+            levels = np.arange(0, 0.00015, 0.000015)
+
+    fig = plt.figure(figsize=[12, 5])
+    ax = fig.add_subplot(
+        111, projection=ccrs.PlateCarree(central_longitude=180))
     clim.sel(season=season).plot.contourf(ax=ax,
-                                          levels=np.arange(0, 13.5, 1.5),
+                                          levels=levels,
                                           extend='max',
                                           transform=ccrs.PlateCarree(),
                                           cbar_kwargs={'label': clim.units},
@@ -44,32 +59,39 @@ def create_plot(clim, model, season, gridlines=False):
     ax.coastlines()
     if gridlines:
         plt.gca().gridlines()
-    
+
     title = f'{model} precipitation climatology ({season})'
     plt.title(title)
 
 
 def main(inargs):
     """Run the program."""
-
     dset = xr.open_dataset(inargs.pr_file)
-    
-    clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
-    clim = convert_pr_units(clim)
 
-    create_plot(clim, dset.attrs['source_id'], inargs.season)
+    clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
+    clim = convert_pr_units(clim, units=inargs.units)
+
+    create_plot(clim, dset.attrs['source_id'], inargs.season,
+                gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
 
 
 if __name__ == '__main__':
-    description='Plot the precipitation climatology.'
+    description = 'Plot the precipitation climatology.'
     parser = argparse.ArgumentParser(description=description)
-    
+
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
-    parser.add_argument("season", type=str, help="Season to plot")
+    parser.add_argument("season", type=str, choices=[
+                        'DJF', 'MAM', 'JJA', 'SON'], help="Season to plot")
     parser.add_argument("output_file", type=str, help="Output file name")
 
+    parser.add_argument("--gridlines", action="store_true", default=False,
+                        help="Include gridlines on the plot")
+    parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
+                        help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--units", type=int, choices=[0, 1, 2], default=1,
+                        help='Change the unit type 0: kgm-2s-1, 1: mm/day, 2: mm/hr ')
+
     args = parser.parse_args()
-    
+
     main(args)
-    
